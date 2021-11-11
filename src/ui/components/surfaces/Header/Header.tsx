d071a551eff92994b76be7158ca5f6f1d7343b33
felipewned@hotmail.com <felipewned@hotmail.com>
2021-11-11 07:09:16 -0300

    Criacao das Informacoes das Diaristas

--- src/ui/components/surfaces/Header/Header.tsx
+++ src/ui/components/surfaces/Header/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { HeaderAppBar } from './Header.style';
+import { HeaderAppBar, HeaderLogo } from './Header.style';
+import { Toolbar, Container } from '@material-ui/core';
 
 const Header: React.FC = () => {
-     return <HeaderAppBar>aaaaa</HeaderAppBar>;
+     return (
+          <HeaderAppBar position={'sticky'}>
+               <Toolbar component={Container}>
+                    <HeaderLogo src="/img/logos/logo.svg" alt="e-diaristas" />
+               </Toolbar>
+          </HeaderAppBar>
+     );
 };
 
 export default Header;
